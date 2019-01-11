4f6f3bcc03d8b74aba6eb7dbcf6b9c8e58a31049
richard <coolwonk@gmail.com>
2019-01-11 12:40:38 -0800

    Set mac address, transmit battery voltage.

--- CWReceive/CWReceive.ino
+++ CWReceive/CWReceive.ino
@@ -18,35 +18,39 @@ extern "C" {
   #include "user_interface.h"
 }
 
-// not used yet RAS
+// fix mac adder so parts can be easily interchanged
 void initVariant() {
   uint8_t mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
-  wifi_set_macaddr(STATION_IF, mac);
+  bool a = wifi_set_macaddr(SOFTAP_IF, &mac[0]);
+  //wifi_set_macaddr(STATION_IF, mac);
 }
 
 WiFiClient client;
-ADC_MODE(ADC_VCC);  // set to measure internal vcc
 volatile boolean haveReading = false;
+volatile int battery_voltage;
 
 void setup()
 {
   Serial.begin(115200);
+  delay(100); // may need to get sign on OK
   Serial.println("\r\nESP_Now MASTER CONTROLLER\r\n");
   pinMode(BLUELED, OUTPUT);
+  initVariant();
   WiFi.mode(WIFI_AP);
-  //initVariant();
-   // This is the mac address of the Slave in AP Mode
+  // This is the mac address of the Slave in AP Mode
   Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
   WiFi.disconnect();
  
   esp_now_init();
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
     
-  esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len)
+  esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len)                     
   {
-      Serial.printf("Got Something length =\t%i\n", len);
+      Serial.printf("Got Something length =\t%i", len);
+      // adc data is 10 bit little endian
+      battery_voltage =(data[0] + (((data[1] & 3) << 8)));
+      Serial.printf(" with value =\t%i\n", battery_voltage);
       haveReading = true;
-  
     // only needed if you want to talk back  
 //      if (!esp_now_is_peer_exist(macaddr))
 //      {
@@ -72,15 +76,26 @@ void loop()
          delay(450);
          Serial.print(".");
          yield();
-         if (i > 20) ESP.restart();
+         if (i > 20) {
+            initVariant();
+            WiFi.mode(WIFI_AP);
+            WiFi.disconnect();
+            ESP.restart();
+         }
       }
       Serial.print("\nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
       // ping ThingSpeak channel and send battery voltage
       ThingSpeak.begin(client);
       ThingSpeak.setField(1, HIGH);
-      ThingSpeak.setField(2, (float)ESP.getVcc() / 833);
+      ThingSpeak.setField(2, ((float)battery_voltage) / 300);
+      Serial.print("Battery voltage: "); Serial.println(((float)battery_voltage) / 300);
       ThingSpeak.writeFields( MYCHANNEL, APIKey);
-      delay(200);
+      //delay(200);
+      // need to set mac again so it will recover with set mac
+      initVariant();
+      WiFi.mode(WIFI_AP);
+      // check again to make sure mac is set correctly
+      Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
       ESP.restart(); // re-enable ESP-NOW     
   }
 }
--- CWTransmit/CWTransmit.ino
+++ CWTransmit/CWTransmit.ino
@@ -5,37 +5,37 @@
 #include <ESP8266WiFi.h>
 extern "C" {
   #include <espnow.h>
+  #include "user_interface.h"
 }
 
 #define WIFI_CHANNEL 4 // The quietest channel I could find
 
 //MAC ADDRESS OF THE DEVICE YOU ARE SENDING TO
-byte remoteMac[] = {0x5e, 0xcf, 0x7f, 0x06, 0x72, 0x6a};
-const byte dataLength=7;
-byte rcvData[dataLength];
-
-//void system_deep_sleep_instant(uint32 time_in_us);
+//byte remoteMac[] = {0x5e, 0xcf, 0x7f, 0x06, 0x72, 0x6a};
+byte remoteMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+const byte dataLength=2;
+ADC_MODE(ADC_VCC);  // set to measure internal vcc
+int battery_voltage;
 
 void setup()
 {
-  //Serial.begin(115200);
+  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin();
   WiFi.disconnect();
-  //delay(2);
   //system_phy_set_max_tpw(20);    //the maximum value of RF Tx Power, unit : 0.25dBm, range [0, 82] 19dBm = 76, 10dBm = 40, 14 dBm = 56
-  //delay(2);
-  //Serial.print("\r\n\r\nEsp Now Transmitter with Device MAC: ");
-  //Serial.println(WiFi.macAddress());
+  Serial.print("\r\n\r\nEsp Now Transmitter with Device MAC: ");
+  Serial.println(WiFi.macAddress());
 
   esp_now_init();
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_add_peer(remoteMac, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);
-  esp_now_send(remoteMac, rcvData, dataLength);
-  //system_deep_sleep_instant(0);
+  battery_voltage = ESP.getVcc();
+  esp_now_send(remoteMac, (u8*)&battery_voltage, dataLength);
+  delay(10); // required for send to complete
+  system_deep_sleep_instant(0);
   
-  //ESP.deepSleepInstant(100000000, RF_NO_CAL);
-  ESP.deepSleep(0);  // shut down, save power
+  //ESP.deepSleep(0);  // shut down, save power
   //delay(100); // Need this to make sleep work???? 
 }  
 
