1309f93bae70614b52cc0aed5efe2262e78ef246
richard <coolwonk@gmail.com>
2020-06-17 16:03:24 -0700

    Simplified code.

--- CWReceive/CWReceive.ino
+++ CWReceive/CWReceive.ino
@@ -1,26 +1,25 @@
 
 // Receiver "Slave"
 // The receiver listens in esp now mode and when a message is received,
-// it will switch to wifi mode to transmit the data to ThingSpeak through the wifi 
+// it will switch to wifi mode to transmit the data to ThingSpeak through the wifi
 // router. Note that the receiver has wall power and does not need to conserve power.
 // ESP Now gateway idea taken from:
 // https://github.com/HarringayMakerSpace/ESP-Now/blob/master/EspNowWatsonRestartingGateway/EspNowWatsonRestartingGateway.ino
 
 #define WIFI_CHANNEL 4  // This seems to be the quietest channel in my area
-//#define BLUELED 1 // do not use TX but drive blue LED instead
-#define BLUELED D0 // NodeMCU version
 
 #include <ESP8266WiFi.h>
 #include "password.h"
 #include "ThingSpeak.h"
 extern "C" {
-  #include <espnow.h>
-  #include "user_interface.h"
+#include <espnow.h>
+#include "user_interface.h"
 }
 
 // fix mac adder so parts can be easily interchanged
 void initVariant() {
   uint8_t mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+  //uint8_t mac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
   WiFi.mode(WIFI_AP);
   bool a = wifi_set_macaddr(SOFTAP_IF, &mac[0]);
 }
@@ -28,75 +27,73 @@ void initVariant() {
 WiFiClient client;
 volatile boolean haveReading = false;
 volatile int battery_voltage;
+volatile int peer_mac;
 
 void setup()
 {
   Serial.begin(115200);
-  //delay(100); // may need to get sign on OK
-  Serial.println("\r\nESP_Now MASTER CONTROLLER\r\n");
-  pinMode(BLUELED, OUTPUT);
+  Serial.println("\r\nESP_Now running\r\n");
+  pinMode( LED_BUILTIN, OUTPUT);
+  digitalWrite( LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite( LED_BUILTIN, HIGH);
+
   initVariant();
 
   // This is the mac address of the Slave in AP Mode
   Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
-  WiFi.disconnect();
-  esp_now_init();
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
-  esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len)                     
-  {
-      haveReading = true;
-      // adc data is 10 bit little endian
-      battery_voltage =(data[0] + (((data[1] & 3) << 8)));
-      //Serial.printf("Got Something length =\t%i", len);
-      //Serial.printf(" with value =\t%i\n", battery_voltage);
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
 
-    // only needed if you want to talk back  
-//      if (!esp_now_is_peer_exist(macaddr))
-//      {
-//        Serial.print("adding peer ");
-//        esp_now_add_peer(mac, ESP_NOW_ROLE_COMBO, WIFI_CHANNEL, NULL, 0);
-//      }
+  if (esp_now_set_self_role(ESP_NOW_ROLE_SLAVE) == 0) {
+    Serial.println("Now I am slave");
+  } else {
+    Serial.println("Error setting role");
+  }
+
+  esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len)
+  {
+    haveReading = true;
+    // adc data is 10 bit little endian
+    battery_voltage = (data[0] + (((data[1] & 3) << 8)));
+    peer_mac = mac[0];
   });
 }
 
 void loop()
 {
   if (haveReading) {
-      haveReading = false;
-      WiFi.mode(WIFI_STA);
-      Serial.print("Connecting to "); Serial.print(ssid);
-      WiFi.begin(ssid, password);
-      int i = 0;
-      while (WiFi.status() != WL_CONNECTED) {
-         i++;
-         digitalWrite(BLUELED, LOW);
-         delay(20);
-         digitalWrite(BLUELED, HIGH);
-         delay(450);
-         Serial.print(".");
-         yield();
-         if (i > 20) {
-            initVariant();
-            WiFi.mode(WIFI_STA);
-            WiFi.disconnect();
-            ESP.restart();
-         }
+    haveReading = false;
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    Serial.print("Connecting to "); Serial.print(ssid);
+    WiFi.begin(ssid, password);
+    int i = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+      i++;
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(20);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(450);
+      Serial.print(".");
+      yield();
+      if (i > 20) {
+        ESP.restart();
       }
-      Serial.print("\nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
-      // ping ThingSpeak channel and send battery voltage
-      ThingSpeak.begin(client);
-      ThingSpeak.setField(1, HIGH);
-      ThingSpeak.setField(2, ((float)battery_voltage) / 300);
-      Serial.print("Battery voltage: "); Serial.println(((float)battery_voltage) / 300);
-      ThingSpeak.writeFields( MYCHANNEL, APIKey);
-      //delay(200);
-      // need to set mac again so it will recover with set mac
-      initVariant();
-      WiFi.mode(WIFI_STA);
-      // check again to make sure mac is set correctly
-      Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
-      ESP.restart(); // re-enable ESP-NOW     
+    }
+    tone(15, 200 + (peer_mac * 10), 200 + (peer_mac * 5));  // generate a unit unique beep on pin gpio 15
+    Serial.print("\nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+    // ping ThingSpeak channel and send battery voltage
+    ThingSpeak.begin(client);
+    ThingSpeak.setField(1, peer_mac); // send up lsb of the mac of the xmiter that signaled
+    ThingSpeak.setField(2, ((float)battery_voltage) / 300);
+    Serial.print("Battery voltage: "); Serial.println(((float)battery_voltage) / 300);
+    ThingSpeak.writeFields( MYCHANNEL, APIKey);
+
+    ESP.restart(); // re-enable ESP-NOW
   }
   delay(100);
 }
-
--- CWTransmit/CWTransmit.ino
+++ CWTransmit/CWTransmit.ino
@@ -1,46 +1,61 @@
 // Transmitter "Master"
+// When powered up, this transmitter will send battery
+// voltage and peer mac address to its peer and then
+// it will immediately go into low power mode.
 // Note that this code should be optimized for low power
 // as the transmitter typically runs on batteries.
 
 #include <ESP8266WiFi.h>
 extern "C" {
-  #include <espnow.h>
-  #include "user_interface.h"
+#include <espnow.h>
+#include "user_interface.h"
 }
 
 #define WIFI_CHANNEL 4 // The quietest channel I could find
 
 //MAC ADDRESS OF THE DEVICE YOU ARE SENDING TO
-byte remoteMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
-const byte dataLength=2;
+//byte remoteMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+byte remoteMac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // this broadcasts to all receivers
+const byte dataLength = 2;
 ADC_MODE(ADC_VCC);  // set to measure internal vcc
 int battery_voltage;
 
 void setup()
 {
   Serial.begin(115200);  // enabling the serial port does not cause extra current drain
-  WiFi.mode(WIFI_STA);
-  WiFi.begin();
-  WiFi.disconnect();
-  //system_phy_set_max_tpw(20);    //the maximum value of RF Tx Power, unit : 0.25dBm, range [0, 82] 19dBm = 76, 10dBm = 40, 14 dBm = 56
   Serial.print("\r\n\r\nEsp Now Transmitter with Device MAC: ");
   Serial.println(WiFi.macAddress());
 
-  esp_now_init();
-  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-  esp_now_add_peer(remoteMac, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);
-  battery_voltage = ESP.getVcc();
-  battery_voltage = ESP.getVcc();  // may need to read twice to be reliable???
-  esp_now_send(remoteMac, (u8*)&battery_voltage, dataLength); // better to send local mac!
-  delay(10); // required for send to complete
-  system_deep_sleep_instant(0);
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    ESP.restart(); // re-enable ESP-NOW
+  }
 
-  //ESP.deepSleep(0);  // It is not known if these last two lines are needed!!
-  //delay(100);
-}  
+  if (esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER) == 0) {
+    Serial.println("Now I am master");
+  } else {
+    Serial.println("Error setting role");
+  }
+
+  if (esp_now_add_peer(remoteMac, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0) == 0) {
+    Serial.println("Added peer");
+  } else {
+    Serial.println("Error adding peer");
+  }
+
+  esp_now_register_send_cb([](uint8_t* mac, uint8_t sendStatus) {
+    Serial.printf("send_cb, send done, status = %i\n", sendStatus);
+    system_deep_sleep_instant(0);
+  });
+  // battery value appears to be a 12 bit value in mv
+  battery_voltage = ESP.getVcc();
+  Serial.printf("bat voltage = %d\n",battery_voltage);
+  
+  esp_now_send(remoteMac, (u8*)&battery_voltage, dataLength);
+  delay(100); // required for send to complete
+  system_deep_sleep_instant(0);
+}
 
 void loop()
 {
-  ESP.deepSleep(0);  // It is not known if these last two lines are needed!!
 }
-
